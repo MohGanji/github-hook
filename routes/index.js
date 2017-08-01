9c1a5c26ead82227027f3b0cc9a43dd262cb091b
MohGanji <mohammad-ganji@outlook.com>
2017-08-01 22:25:38 +0430

    created a webhook to test

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var exec = require('child_process').exec;
+var express = require('express');
+var router = express.Router();
+var configs = require('./../configs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send(200, "OK");
+});
+
+router.post('/', function(req, res, next){
+    var serverToken = req.header("X-Hub-Signature");
+    var gitEvent = req.header("X-GitHub-Event");
+  
+    // if(serverToken && serverToken != configs.secretToken){
+    //   res.send(403, "unAuthorized token");
+    // }
+    
+    var cmd = configs.shellCommand;
+    if(gitEvent == "push"){
+      new Promise(function(fulfill, reject){
+          var child = exec(cmd, function(error, stdout, stderr){
+          console.log("stdout: ", stdout);
+          console.log("stderr: ", stderr);
+          console.log("error: ", error);
+        });
+        fulfill();
+      }).then(res.send(200, "Ok"));
+    }
+
+    // res.send(200, "Ok");
+});
+
+module.exports = router;
